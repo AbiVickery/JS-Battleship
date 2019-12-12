c736f0f20dc13653974b23857ebeaff8b1f1c8d8
Abi Vickery <vickeryabi@gmail.com>
2019-12-12 13:18:33 -0600

    gameboard wip

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-debugger
+// You should add the tiles of gameboard to the dom
+// The AI must select a random tile on the gameboard
+// You will select tiles and the game will indicate a HIT or a miss
+// On HIT the game is over
+// Add a button to start a new game
