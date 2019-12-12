b4b7b47459bb451d793153442a6fc35c6dfdc458
Abi Vickery <vickeryabi@gmail.com>
2019-12-12 13:26:16 -0600

    more notes

--- js/app.js
+++ js/app.js
@@ -3,3 +3,16 @@
 // You will select tiles and the game will indicate a HIT or a miss
 // On HIT the game is over
 // Add a button to start a new game
+
+// Add x amount of tiles to gameboard
+// Add event listener to each tile
+
+// listener
+// check if this is AI block
+// if it is you win and end game
+// else you missed
+// keep going
+
+// button listener
+// reset gameboard and have AI choose new tile
+
